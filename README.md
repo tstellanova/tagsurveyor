fee5b186711b542a459b94bbd9af794f3330cb47
Todd Stellanova <tstellanova@users.noreply.github.com>
2021-07-20 09:25:31 -0700

    Add support for SHT3x temp and humidity sensor, improve BLE beacon filtering (#1)
    * further support for BLE scanning
    * use external beacon scanner config
    * support additional beacon types
    * initial support for SHT3x m8 sensor
    * Select Eddystone, update readme

--- README.md
+++ README.md
@@ -1,12 +1,42 @@
 
-# Particle Tracker Edge Firmware
 
-Application reference firmware for the Particle Tracker.
+## Tag Surveyor
+
+This Tracker Edge-based demo listens for BLE tags / beacons that advertise
+identifying information.  It collects a list of tags it has seen
+and forwards that to the Particle Cloud periodically, when the Tracker device has
+a geolocation and a Particle Cloud connection. 
+
+In addition, this app can collect temperature and humidity information
+from an attached Tracker M8 
+[Temperature/Humidity sensor](https://docs.particle.io/datasheets/asset-tracking/m8-temperature-humidity/) 
+
+This app allows you to correlate the location of the Tracker with 
+the tags it has seen and the local temperature and humidity conditions. 
+This may be useful for eg warehouse or temperature-controlled inventory management.
+
+The full source code is available in the 
+[tagsurveyor github repo](https://github.com/tstellanova/tagsurveyor)
+
+Here's a view of how this app reports beacons in the Particle Console:
+![Tag Surveyor console screenshot](./img/tagsurveyor-console.png)
+
+
+### Tracker Safe Shipping
+Note that for safe shipping purposes, this app supports the Tracker shipping
+mode command: From the Particle Console, while the device is connected to
+the cloud, you can send the following from the "cmd" function: `{"cmd":"enter_shipping"}`
+This will cause the Tracker to power off and disconnect the battery
+so that the device will not awaken during shipment. 
+Connecting the Tracker to USB 
+[will reawaken it](see https://docs.particle.io/tutorials/asset-tracking/setup/#powering-off).
+ 
+
 
 # Resources
 
-- [Latest Release](https://github.com/particle-iot/tracker-edge/releases)
-- [Changelog](CHANGELOG.md)
+- [Tracker Edge Latest Release](https://github.com/particle-iot/tracker-edge/releases)
+- [Tracker Edge Changelog](CHANGELOG.md)
 
 ### CREDITS AND ATTRIBUTIONS
 
--- src/main.cpp
+++ src/main.cpp
@@ -13,35 +13,145 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
 
 #include "Particle.h"
 
 #include "tracker_config.h"
 #include "tracker.h"
 
+#include "BeaconScanner.h"
+// Robust temperature / humidity sensor
+#include "sht3x-i2c.h"
+
+
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
 PRODUCT_ID(TRACKER_PRODUCT_ID);
-PRODUCT_VERSION(TRACKER_PRODUCT_VERSION);
+PRODUCT_VERSION(TRACKER_PRODUCT_VERSION + 1);
 
 STARTUP(
     Tracker::startup();
 );
 
-SerialLogHandler logHandler(115200, LOG_LEVEL_TRACE, {
-    { "app.gps.nmea", LOG_LEVEL_INFO },
-    { "app.gps.ubx",  LOG_LEVEL_INFO },
+SerialLogHandler logHandler(115200, LOG_LEVEL_INFO, {
+    { "app.gps.nmea", LOG_LEVEL_WARN },
+    { "comm.protocol", LOG_LEVEL_WARN},
+    { "mux", LOG_LEVEL_WARN},
+    { "app.gps.ubx",  LOG_LEVEL_WARN },
     { "ncp.at", LOG_LEVEL_INFO },
     { "net.ppp.client", LOG_LEVEL_INFO },
 });
 
-void setup()
-{
-    Tracker::instance().init();
+
+// M8 Temperature - Humidity sensor
+Sht3xi2c sht_sensor(Wire3);
+
+static double last_temperature = 0;
+static double last_humidity = 0;
+static uint32_t last_sht_read_ms = 0;
+
+// Read the current M8 sensor data
+static void read_sht_data() {
+    double temp, humid;
+    if (0 == sht_sensor.get_reading(&temp, &humid)) {
+        last_temperature = temp;
+        last_humidity = humid;
+        last_sht_read_ms = millis();
+    }
 }
 
-void loop()
-{
-    Tracker::instance().loop();
+// Read and publish temperature and humidity sensor data
+static void publish_sht_data(JSONWriter& writer) {
+    if (last_sht_read_ms > 0) {
+        writer.name("sht3_temp").value(last_temperature);
+        writer.name("sht3_humid").value(last_humidity);
+    }
 }
+
+static void publish_beacon_data(JSONWriter& writer) {
+        writer.name("bcnz").beginObject();
+ 
+    #ifdef SUPPORT_LAIRDBT510
+    {
+        auto beacons = Scanner.getLairdBt510();
+        Log.trace("Laird avail: %d", beacons.size());
+
+        if (beacons.size() > 0) {
+            for (auto beacon : beacons) {
+                Log.info("L beacon: %s", beacon.getAddress().toString().c_str());
+                beacon.toJson(&writer);
+            }
+        }
+    }
+    #endif //SUPPORT_LAIRDBT510
+
+    #ifdef SUPPORT_EDDYSTONE
+    {
+        auto beacons = Scanner.getEddystone();
+        Log.trace("Eddy avail: %d", beacons.size());
+
+        if (beacons.size() > 0) {
+            for (auto beacon : beacons) {
+                Log.info("E beacon: %s", beacon.getAddress().toString().c_str());
+                beacon.toJson(&writer);
+            }
+        }
+    }
+    #endif //SUPPORT_EDDYSTONE
+
+     #ifdef SUPPORT_IBEACON
+    {
+        auto beacons = Scanner.getiBeacons();
+        Log.trace("iBeacons avail: %d", beacons.size());
+
+        if (beacons.size() > 0) {
+            for (auto beacon : beacons) {
+                Log.info("I beacon: %s", beacon.getAddress().toString().c_str());
+                beacon.toJson(&writer);
+            }
+        }
+    }
+    #endif //SUPPORT_IBEACON
+
+    writer.endObject(); //bcnz
+}
+
+// Called by Tracker instance when it's collecting `loc` json for publication
+static void locGenCallback(JSONWriter& writer, LocationPoint& point, const void *context) {
+    publish_sht_data(writer);
+    publish_beacon_data(writer);
+}
+
+void setup() {
+    Tracker::instance().init();
+    delay(500);
+        
+    // Turn on 5V output on M8 connector
+    pinMode(CAN_PWR, OUTPUT);      
+    digitalWrite(CAN_PWR, HIGH);  
+    delay(500);
+
+    Tracker::instance().location.regLocGenCallback(locGenCallback);
+    // enable BLE radio so we can use it for scanning
+    if ( SYSTEM_ERROR_NONE != BLE.on()) {
+        Log.error("BLE on failed");
+    }
+
+    // kick off the M8 sensor reading
+    sht_sensor.begin(CLOCK_SPEED_400KHZ);
+    int rc = sht_sensor.start_periodic(SHT31_ACCURACY_MEDIUM, 1); // 1 Hz measurement
+    if (0 != rc) {
+        Log.error("SHT3x failed to start: %d",rc);
+    }
+
+}
+
+void loop() {
+    Tracker::instance().loop();
+    // scan for nearby BLE beacons advertising
+    Scanner.scan(5, SCAN_LAIRDBT510 | SCAN_EDDYSTONE | SCAN_IBEACON );
+    // record the latest M8 sensor readings
+    read_sht_data();
+} 
